21239845bebb4a6e0aae2cebfafb0fc35ca65606
Tsygankovae.a <Tsygankovae.a@outlook.com>
2021-11-27 19:24:02 +0300

    Добавьте файлы проекта.

---
+++ Lab4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        //Находим квадрат целого числа.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число от 1 до N"); 
+            int N2 = Convert.ToInt32(Console.ReadLine());
+            int N = 0 ;
+            for (int i = 1; i < (N2+1) ; i++)
+            {               
+                N = N + (2 * i- 1);
+                Console.WriteLine("Квадрат числа {0} равен {1}", i, N);
+            }
+            Console.ReadKey();
+        }
+    }
+}
